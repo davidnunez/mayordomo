dfab4ecdde0e9d50e1cc721dfc1d24af25691ffd
David Nunez <david@davidnunez.com>
2013-09-21 21:10:09 -0400

    adjustments to script

---
+++ inbox_to_omnifocus.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+require "shellwords"
+
+filename = ARGV[0]
+
+def add_task (task, note)
+	cmd = "~/projects/OTask/bin/otask #{task.shellescape}"
+	if note != ""
+		cmd = "echo #{note.shellescape} | " + cmd
+	end
+	system(cmd)
+end
+
+
+task = ""
+note = ""
+File.open( filename ).each do |line|
+	if line.start_with?('-')
+		if task != ""
+			add_task(task, note)
+			task = ""
+			note = ""
+		end
+
+		line[0..1] = '' 
+		splitline = line.split('|')
+		creation_date = splitline[0]
+		type = splitline[1] 
+		title = splitline[2..splitline.length].join(' | ') 
+		creation_date.strip!
+		creation_date[-5,1] = ' '
+		#puts creation_date + " TYPE: #{type.strip}"
+		task = (title.strip! + " c(#{creation_date})")
+		# puts task.shellescape
+		
+	else
+		note += line
+	end
+end
+
+add_task(task, note)
+
