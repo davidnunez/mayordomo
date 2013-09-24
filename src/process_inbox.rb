70347fffbfddaaee94ea7bb12a212327139f80a3
David Nunez <david@davidnunez.com>
2013-09-24 18:16:31 -0400

    Use Task as tree node "name" and not content

--- src/process_inbox.rb
+++ src/process_inbox.rb
@@ -13,7 +13,7 @@ def _recurse_tree(parent, depth, file)
         if tabs < depth
             break
         end
-        node = Tree::TreeNode.new(task.task_string, task)
+        node = Tree::TreeNode.new(task)
         if tabs >= depth
             parent << node
             last_line = _recurse_tree(node, task.indent_level+1, file)
@@ -23,6 +23,8 @@ def _recurse_tree(parent, depth, file)
 end
 
 filename = ARGV[0]
-root_node = Tree::TreeNode.new("ROOT", "Root Content")
+root_node = Tree::TreeNode.new("ROOT")
 _recurse_tree(root_node, 0, File.open(filename))
 root_node.print_tree
+
+root_node.each {|node|  puts node.name  if (node.name.is_a? Task and node.name.is_project?)}
