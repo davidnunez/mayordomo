dcb68247c3a538911f8dc43f64fcb333e82f7783
David Nunez <david@davidnunez.com>
2014-01-09 22:40:45 -0500

    refactored calendar

--- src/calendar.rb
+++ src/calendar.rb
@@ -1,46 +1,36 @@
 require 'chronic'
 require 'google_calendar'
 require 'active_support/time'
-
+require 'parseconfig'
 
 module Mayordomo
 	class Calendar
 		include Singleton
-		config = ParseConfig.new('main.config')
-		def maintenance
-			if @cal_maintenance == nil
-				@cal_maintenance = generate_calendar('z qs maintenance')
-			end
-			return @cal_maintenance
+		attr_accessor :short_names
+
+		def get_calendar_by_short_name(_short_name)
+			return get_calendar(@short_names[_short_name])
 		end
 
-		def consumption
-			if @cal_consumption == nil
-				@cal_consumption = generate_calendar('z qs consumption')
-			end
-			return @cal_consumption
-		end
+		def get_calendar(_calendar)
+			return @calendars[_calendar] if @calendars.has_key?(_calendar)
+#			@config = ParseConfig.new('main.config')
 
-		def moves_places
-			if @cal_moves_places == nil
-				@cal_moves_places = generate_calendar('z qs moves places')
-			end
-			return @cal_moves_places
-		end
-
-		def moves_travel
-			if @cal_moves_travel == nil
-				@cal_moves_travel = generate_calendar('z qs moves travel')
-			end
-			return @cal_moves_travel
-		end
-		def generate_calendar(_calendar)
-			config = ParseConfig.new('main.config')
-
-			return Google::Calendar.new(:username => config['CALENDAR_USERNAME'],
-				:password => config['CALENDAR_PASSWORD'],
-				:app_name => config['CALENDAR_APP_NAME'],
+			@calendars[_calendar] = Google::Calendar.new(:username => @config['CALENDAR_USERNAME'],
+				:password => @config['CALENDAR_PASSWORD'],
+				:app_name => @config['CALENDAR_APP_NAME'],
 				:calendar => _calendar)
+			return @calendars[_calendar]
+		end
+		def initialize
+			@config = ParseConfig.new('main.config')
+			@short_names = Hash[
+					"maintenance"  => @config['CALENDAR_NAME_MAINTENANCE'],
+					"consumption"  => @config['CALENDAR_NAME_CONSUMPTION'],
+					"moves_places" => @config['CALENDAR_NAME_MOVES_PLACES'],
+					"moves_travel" => @config['CALENDAR_NAME_MOVES_TRAVEL'],
+				]
+			@calendars = Hash.new
 		end
 
 	end
