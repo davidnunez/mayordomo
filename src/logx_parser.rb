840043834adcc0afc08123a2e90b58562b4fe132
David Nunez <david@davidnunez.com>
2014-02-07 00:09:50 -0500

    code inspection fixes

--- src/logx_parser.rb
+++ src/logx_parser.rb
@@ -4,14 +4,14 @@ require 'oauth2'
 require 'chronic'
 require 'google_calendar'
 require 'active_support/time'
-require ('./task.rb')
+require 'task'
 require 'imdb'
-require './calendar.rb'
+require 'calendar'
 
 filename = ARGV[0]
 
 
-cal_consumption = Mayordomo::Calendar.instance.get_calendar_by_short_name("consumption")
+cal_consumption = Mayordomo::Calendar.instance.get_calendar_by_short_name('consumption')
 
 
 text=File.open(filename).read
@@ -21,8 +21,8 @@ text.each_line do |line|
 	puts "processing #{line}"
 	task = Task.new(line)
 	next if task.is_done?
-	if task.includes_tag?("movie") or task.includes_tag?("tv")
-		puts "--------------"
+	if task.includes_tag?('movie') or task.includes_tag?('tv')
+		puts '--------------'
 		puts line
 		i = Imdb::Search.new(task.title)
 		i.movies[0..5].each_with_index do |movie, index|
@@ -40,14 +40,14 @@ text.each_line do |line|
 		selection = selection.to_i
 
 		season = nil
-		if task.includes_tag?("tv")
-			puts "Season and Episode?"
+		if task.includes_tag?('tv')
+			puts 'Season and Episode?'
 			season = $stdin.gets.chomp
 		end
 		puts "Adding #{i.movies[selection].title} to calendar..."
-		event = cal_consumption.create_event do |e|
+		cal_consumption.create_event do |e|
 		  e.title = i.movies[selection].title 
-		  e.title = e.title + " " + season if season
+		  e.title = e.title + ' ' + season if season
 		  e.content = i.movies[selection].id
 		  e.start_time = Chronic.parse(task.creation_date)
 		  e.end_time = Chronic.parse(task.creation_date) + i.movies[selection].length.to_i * 60
